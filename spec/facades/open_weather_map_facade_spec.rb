d190dad3ed6cfc1c054318e42ecd6f027f818015
Leland Curtis <lelandcurtis88@gmail.com>
2022-03-06 16:54:10 -0700

    Write openweathermapfacade test.

---
+++ spec/facades/open_weather_map_facade_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.describe 'Open Weather Map Facade' do
+  context 'successful query' do
+    describe 'get_forecast', :vcr do
+      let!(:lat) { 39.738453 }
+      let!(:lon) { -104.984853 }
+      let!(:forecast) { OpenWeatherMapFacade.get_forecast(lat, lon) }
+
+      it "returns a forecast object" do
+        expect(forecast).to be_a(Forecast)
+      end
+
+      it "returns a forecast object with the correct current weather object and data" do
+        current = forecast.current_weather
+        expect(current).to be_a(CurrentWeather)
+        expect(current.datetime).to be_a(DateTime)
+        expect(current.sunrise).to be_a(DateTime)
+        expect(current.sunset).to be_a(DateTime)
+        expect(current.temperature).to be_a(Float)
+        expect(current.feels_like).to be_a(Float)
+        expect(current.humidity).to be_a(Float || Int)
+        expect(current.uvi).to be_a(Float || Int)
+        expect(current.visibility).to be_a(Float || Int)
+        expect(current.conditions).to be_a(String)
+        expect(current.icon).to be_a(String)
+      end
+
+      it "returns a forecast object with the correct daily weather array, objects and data" do
+        daily_array = forecast.daily_weather
+        expect(daily_array).to be_a(Array)
+        expect(daily_array.count).to eq(5)
+        daily = daily_array.sample(1)[0]
+        expect(daily).to be_a(DailyWeather)
+        expect(daily.date).to be_a(Date)
+        expect(daily.sunrise).to be_a(DateTime)
+        expect(daily.sunset).to be_a(DateTime)
+        expect(daily.temperature).to be_a(Float)
+        expect(daily.feels_like).to be_a(Float)
+        expect(daily.humidity).to be_a(Float || Int)
+        expect(daily.uvi).to be_a(Float || Int)
+        expect(daily.visibility).to be_a(Float || Int)
+        expect(daily.conditions).to be_a(String)
+        expect(daily.icon).to be_a(String)
+      end
+
+      it "returns a forecast object with the correct hourly weather array, objects and data" do
+        hourly_array = forecast.hourly_weather
+        expect(hourly_array).to be_a(Array)
+        expect(hourly_array.count).to eq(8)
+        hourly = hourly_array.sample(1)[0]
+        expect(hourly).to be_a(HourlyWeather)
+        expect(hourly.time).to be_a(Time)
+        expect(hourly.temperature).to be_a(Float)
+        expect(hourly.conditions).to be_a(String)
+        expect(hourly.icon).to be_a(String)
+      end
+    end
+  end
+end
