31e206425e38366d27ff115ed20cfc1de9322b42
Leland Curtis <lelandcurtis88@gmail.com>
2022-03-06 19:06:35 -0700

    Write get_forecast method for openweathermapfacade.

---
+++ app/facades/open_weather_map_facade.rb
@@ -0,0 +1,55 @@
+class OpenWeatherMapFacade
+  class << self
+
+    def get_forecast(lat, lon)
+      response = OpenWeatherMapService.get_forecast(lat, lon)
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      current_weather = CurrentWeather.new(clean_current_weather_data(json[:current]))
+      hourly_weather = json[:hourly].map do |hour|
+        HourlyWeather.new(clean_hourly_weather_data(hour))
+      end
+      daily_weather = json[:daily].map do |day|
+        DailyWeather.new(clean_daily_weather_data(day))
+      end
+      Forecast.new(current_weather, daily_weather, hourly_weather)
+    end
+
+    def clean_current_weather_data(data)
+      output_data = {}
+      output_data[:dt] = data[:dt])
+      output_data[:sunrise] = data[:sunrise])
+      output_data[:sunset] = data[:sunset])
+      output_data[:temperature] = data[:temp]
+      output_data[:feels_like] = data[:feels_like]
+      output_data[:humidity] = data[:humidity]
+      output_data[:uvi] = data[:uvi]
+      output_data[:visibility] = data[:visibility]
+      output_data[:conditions] = data[:weather][:description]
+      output_data[:icon] = data[:weather][:icon]
+      output_data
+    end
+
+    def clean_hourly_weather_data(data)
+      output_data = {}
+      output_data[:dt] = data[:dt])
+      output_data[:temperature] = data[:temp]
+      output_data[:conditions] = data[:weather][:description]
+      output_data[:icon] = data[:weather][:icon]
+      output_data
+    end
+
+    def clean_daily_weather_data(data)
+      output_data = {}
+      output_data[:dt] = data[:dt])
+      output_data[:sunrise] = data[:sunrise])
+      output_data[:sunset] = data[:sunset])
+      output_data[:max_temp] = data[:temp][:max]
+      output_data[:min_temp] = data[:temp][:min]
+      output_data[:conditions] = data[:weather][:description]
+      output_data[:icon] = data[:weather][:icon]
+      output_data
+    end
+
+  end
+end
