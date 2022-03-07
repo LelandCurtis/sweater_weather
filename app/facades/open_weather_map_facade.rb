753413ae3a36ba3769a2b2f088561051c18d2045
Leland Curtis <lelandcurtis88@gmail.com>
2022-03-06 19:22:40 -0700

    Complete OpenWeatherMapFacade and update VCRs.

--- app/facades/open_weather_map_facade.rb
+++ app/facades/open_weather_map_facade.rb
@@ -6,10 +6,10 @@ class OpenWeatherMapFacade
       json = JSON.parse(response.body, symbolize_names: true)
 
       current_weather = CurrentWeather.new(clean_current_weather_data(json[:current]))
-      hourly_weather = json[:hourly].map do |hour|
+      hourly_weather = json[:hourly][0..7].map do |hour|
         HourlyWeather.new(clean_hourly_weather_data(hour))
       end
-      daily_weather = json[:daily].map do |day|
+      daily_weather = json[:daily][0..4].map do |day|
         DailyWeather.new(clean_daily_weather_data(day))
       end
       Forecast.new(current_weather, daily_weather, hourly_weather)
@@ -17,37 +17,37 @@ class OpenWeatherMapFacade
 
     def clean_current_weather_data(data)
       output_data = {}
-      output_data[:dt] = data[:dt])
-      output_data[:sunrise] = data[:sunrise])
-      output_data[:sunset] = data[:sunset])
+      output_data[:dt] = data[:dt]
+      output_data[:sunrise] = data[:sunrise]
+      output_data[:sunset] = data[:sunset]
       output_data[:temperature] = data[:temp]
       output_data[:feels_like] = data[:feels_like]
       output_data[:humidity] = data[:humidity]
       output_data[:uvi] = data[:uvi]
       output_data[:visibility] = data[:visibility]
-      output_data[:conditions] = data[:weather][:description]
-      output_data[:icon] = data[:weather][:icon]
+      output_data[:conditions] = data[:weather][0][:description]
+      output_data[:icon] = data[:weather][0][:icon]
       output_data
     end
 
     def clean_hourly_weather_data(data)
       output_data = {}
-      output_data[:dt] = data[:dt])
+      output_data[:dt] = data[:dt]
       output_data[:temperature] = data[:temp]
-      output_data[:conditions] = data[:weather][:description]
-      output_data[:icon] = data[:weather][:icon]
+      output_data[:conditions] = data[:weather][0][:description]
+      output_data[:icon] = data[:weather][0][:icon]
       output_data
     end
 
     def clean_daily_weather_data(data)
       output_data = {}
-      output_data[:dt] = data[:dt])
-      output_data[:sunrise] = data[:sunrise])
-      output_data[:sunset] = data[:sunset])
+      output_data[:dt] = data[:dt]
+      output_data[:sunrise] = data[:sunrise]
+      output_data[:sunset] = data[:sunset]
       output_data[:max_temp] = data[:temp][:max]
       output_data[:min_temp] = data[:temp][:min]
-      output_data[:conditions] = data[:weather][:description]
-      output_data[:icon] = data[:weather][:icon]
+      output_data[:conditions] = data[:weather][0][:description]
+      output_data[:icon] = data[:weather][0][:icon]
       output_data
     end
 
--- spec/facades/open_weather_map_facade_spec.rb
+++ spec/facades/open_weather_map_facade_spec.rb
@@ -12,43 +12,43 @@ RSpec.describe 'Open Weather Map Facade' do
       end
 
       it "returns a forecast object with the correct current weather object and data" do
-        current = forecast.current_weather
+        current = forecast.current
         expect(current).to be_a(CurrentWeather)
-        expect(current.datetime).to be_a(DateTime)
-        expect(current.sunrise).to be_a(DateTime)
-        expect(current.sunset).to be_a(DateTime)
+        expect(current.datetime).to be_a(String)
+        expect(current.sunrise).to be_a(String)
+        expect(current.sunset).to be_a(String)
         expect(current.temperature).to be_a(Float)
         expect(current.feels_like).to be_a(Float)
-        expect(current.humidity).to be_a(Float || Integer)
-        expect(current.uvi).to be_a(Float || Integer)
-        expect(current.visibility).to be_a(Float || Integer)
+        expect(current.humidity).to be_a_kind_of(Numeric)
+        expect(current.uvi).to be_a_kind_of(Numeric)
+        expect(current.visibility).to be_a_kind_of(Numeric)
         expect(current.conditions).to be_a(String)
         expect(current.icon).to be_a(String)
       end
 
       it "returns a forecast object with the correct daily weather array, objects and data" do
-        daily_array = forecast.daily_weather
+        daily_array = forecast.daily
         expect(daily_array).to be_a(Array)
         expect(daily_array.count).to eq(5)
         daily = daily_array.sample(1)[0]
         expect(daily).to be_a(DailyWeather)
-        expect(daily.date).to be_a(Date)
-        expect(daily.sunrise).to be_a(DateTime)
-        expect(daily.sunset).to be_a(DateTime)
-        expect(daily.max_temp).to be_a(Float || Integer)
-        expect(daily.min_temp).to be_a(Float || Integer)
+        expect(daily.date).to be_a(String)
+        expect(daily.sunrise).to be_a(String)
+        expect(daily.sunset).to be_a(String)
+        expect(daily.max_temp).to be_a_kind_of(Numeric)
+        expect(daily.min_temp).to be_a_kind_of(Numeric)
         expect(daily.conditions).to be_a(String)
         expect(daily.icon).to be_a(String)
       end
 
       it "returns a forecast object with the correct hourly weather array, objects and data" do
-        hourly_array = forecast.hourly_weather
+        hourly_array = forecast.hourly
         expect(hourly_array).to be_a(Array)
         expect(hourly_array.count).to eq(8)
         hourly = hourly_array.sample(1)[0]
         expect(hourly).to be_a(HourlyWeather)
-        expect(hourly.time).to be_a(Time)
-        expect(hourly.temperature).to be_a(Float || Integer)
+        expect(hourly.time).to be_a(String)
+        expect(hourly.temperature).to be_a_kind_of(Numeric)
         expect(hourly.conditions).to be_a(String)
         expect(hourly.icon).to be_a(String)
       end
